f1348f75a2cd92acaf914b8b63c0924e1d9b5a0a
saimedhi <saimedhi@users.noreply.github.com>
2024-07-10 03:55:12 +0000

    Updated opensearch-php to reflect the latest OpenSearch API spec (2024-07-10)
    Signed-off-by: GitHub <noreply@github.com>

--- src/OpenSearch/Client.php
+++ src/OpenSearch/Client.php
@@ -34,7 +34,7 @@ use OpenSearch\Namespaces\DataFrameTransformDeprecatedNamespace;
 use OpenSearch\Namespaces\IndicesNamespace;
 use OpenSearch\Namespaces\IngestNamespace;
 use OpenSearch\Namespaces\KnnNamespace;
-use OpenSearch\Namespaces\MachineLearningNamespace;
+use OpenSearch\Namespaces\MlNamespace;
 use OpenSearch\Namespaces\MonitoringNamespace;
 use OpenSearch\Namespaces\NodesNamespace;
 use OpenSearch\Namespaces\NotificationsNamespace;
@@ -119,6 +119,11 @@ class Client
     protected $knn;
 
     /**
+     * @var MlNamespace
+     */
+    protected $ml;
+
+    /**
      * @var MonitoringNamespace
      */
     protected $monitoring;
@@ -183,10 +188,6 @@ class Client
      */
     protected $transforms;
 
-    /**
-     * @var MachineLearningNamespace
-     */
-    protected $ml;
 
     /**
      * Client constructor
@@ -207,6 +208,7 @@ class Client
         $this->indices = new IndicesNamespace($transport, $endpoint);
         $this->ingest = new IngestNamespace($transport, $endpoint);
         $this->knn = new KnnNamespace($transport, $endpoint);
+        $this->ml = new MlNamespace($transport, $endpoint);
         $this->monitoring = new MonitoringNamespace($transport, $endpoint);
         $this->nodes = new NodesNamespace($transport, $endpoint);
         $this->notifications = new NotificationsNamespace($transport, $endpoint);
@@ -218,7 +220,6 @@ class Client
         $this->snapshot = new SnapshotNamespace($transport, $endpoint);
         $this->sql = new SqlNamespace($transport, $endpoint);
         $this->ssl = new SslNamespace($transport, $endpoint);
-        $this->ml = new MachineLearningNamespace($transport, $endpoint);
         $this->tasks = new TasksNamespace($transport, $endpoint);
         $this->transforms = new TransformsNamespace($transport, $endpoint);
 
@@ -324,6 +325,43 @@ class Client
         $endpointBuilder = $this->endpoints;
         $endpoint = $endpointBuilder('Count');
         $endpoint->setParams($params);
+        $endpoint->setIndex($index);
+        $endpoint->setBody($body);
+
+        return $this->performRequest($endpoint);
+    }
+    /**
+     * Creates a new document in the index.Returns a 409 response when a document with a same ID already exists in the index.
+     *
+     * $params['id']                     = (string) Unique identifier for the document. (Required)
+     * $params['index']                  = (string) Name of the data stream or index to target. If the target doesn’t exist and matches the name or wildcard (`*`) pattern of an index template with a `data_stream` definition, this request creates the data stream. If the target doesn’t exist and doesn’t match a data stream template, this request creates the index. (Required)
+     * $params['pipeline']               = (string) ID of the pipeline to use to preprocess incoming documents.If the index has a default ingest pipeline specified, then setting the value to `_none` disables the default ingest pipeline for this request.If a final pipeline is configured it will always run, regardless of the value of this parameter.
+     * $params['refresh']                = (enum) If `true`, OpenSearch refreshes the affected shards to make this operation visible to search, if `wait_for` then wait for a refresh to make this operation visible to search, if `false` do nothing with refreshes.Valid values: `true`, `false`, `wait_for`. (Options = true,false,wait_for)
+     * $params['routing']                = (string) Custom value used to route operations to a specific shard.
+     * $params['timeout']                = (string) Period the request waits for the following operations: automatic index creation, dynamic mapping updates, waiting for active shards.
+     * $params['version']                = (number) Explicit version number for concurrency control.The specified version must match the current version of the document for the request to succeed.
+     * $params['version_type']           = (enum) Specific version type: `external`, `external_gte`. (Options = internal,external,external_gte,force)
+     * $params['wait_for_active_shards'] = (any) The number of shard copies that must be active before proceeding with the operation.Set to `all` or any positive integer up to the total number of shards in the index (`number_of_replicas+1`).
+     * $params['pretty']                 = (boolean) Whether to pretty format the returned JSON response.
+     * $params['human']                  = (boolean) Whether to return human readable values for statistics.
+     * $params['error_trace']            = (boolean) Whether to include the stack trace of returned errors.
+     * $params['source']                 = (string) The URL-encoded request definition. Useful for libraries that do not accept a request body for non-POST requests.
+     * $params['filter_path']            = (any) Comma-separated list of filters used to reduce the response.
+     * $params['body']                   = (array) The document (Required)
+     *
+     * @param array $params Associative array of parameters
+     * @return array
+     */
+    public function create(array $params = [])
+    {
+        $id = $this->extractArgument($params, 'id');
+        $index = $this->extractArgument($params, 'index');
+        $body = $this->extractArgument($params, 'body');
+
+        $endpointBuilder = $this->endpoints;
+        $endpoint = $endpointBuilder('Create');
+        $endpoint->setParams($params);
+        $endpoint->setId($id);
         $endpoint->setIndex($index);
         $endpoint->setBody($body);
 
@@ -1599,59 +1637,6 @@ class Client
         return $this->performRequest($endpoint);
     }
     /**
-     * Proxy function to createPointInTime() to prevent BC break.
-     * This API will be removed in a future version. Use 'createPit' API instead.
-     */
-    public function createPointInTime(array $params = [])
-    {
-        return $this->createPit($params);
-    }
-    /**
-         * Creates a new document in the index.Returns a 409 response when a document with a same ID already exists in the index.
-         *
-         * $params['id']                     = (string) Unique identifier for the document. (Required)
-         * $params['index']                  = (string) Name of the data stream or index to target. If the target doesn’t exist and matches the name or wildcard (`*`) pattern of an index template with a `data_stream` definition, this request creates the data stream. If the target doesn’t exist and doesn’t match a data stream template, this request creates the index. (Required)
-         * $params['pipeline']               = (string) ID of the pipeline to use to preprocess incoming documents.If the index has a default ingest pipeline specified, then setting the value to `_none` disables the default ingest pipeline for this request.If a final pipeline is configured it will always run, regardless of the value of this parameter.
-         * $params['refresh']                = (enum) If `true`, OpenSearch refreshes the affected shards to make this operation visible to search, if `wait_for` then wait for a refresh to make this operation visible to search, if `false` do nothing with refreshes.Valid values: `true`, `false`, `wait_for`. (Options = true,false,wait_for)
-         * $params['routing']                = (string) Custom value used to route operations to a specific shard.
-         * $params['timeout']                = (string) Period the request waits for the following operations: automatic index creation, dynamic mapping updates, waiting for active shards.
-         * $params['version']                = (number) Explicit version number for concurrency control.The specified version must match the current version of the document for the request to succeed.
-         * $params['version_type']           = (enum) Specific version type: `external`, `external_gte`. (Options = internal,external,external_gte,force)
-         * $params['wait_for_active_shards'] = (any) The number of shard copies that must be active before proceeding with the operation.Set to `all` or any positive integer up to the total number of shards in the index (`number_of_replicas+1`).
-         * $params['pretty']                 = (boolean) Whether to pretty format the returned JSON response.
-         * $params['human']                  = (boolean) Whether to return human readable values for statistics.
-         * $params['error_trace']            = (boolean) Whether to include the stack trace of returned errors.
-         * $params['source']                 = (string) The URL-encoded request definition. Useful for libraries that do not accept a request body for non-POST requests.
-         * $params['filter_path']            = (any) Comma-separated list of filters used to reduce the response.
-         * $params['body']                   = (array) The document (Required)
-         *
-         * @param array $params Associative array of parameters
-         * @return array
-         */
-    public function create(array $params = [])
-    {
-        $id = $this->extractArgument($params, 'id');
-        $index = $this->extractArgument($params, 'index');
-        $body = $this->extractArgument($params, 'body');
-
-        $endpointBuilder = $this->endpoints;
-        $endpoint = $id ? $endpointBuilder('Create') : $endpointBuilder('Index');
-        $endpoint->setParams($params);
-        $endpoint->setId($id);
-        $endpoint->setIndex($index);
-        $endpoint->setBody($body);
-
-        return $this->performRequest($endpoint);
-    }
-    /**
-     * Proxy function to deletePointInTime() to prevent BC break.
-     * This API will be removed in a future version. Use 'deletePit' API instead.
-     */
-    public function deletePointInTime(array $params = [])
-    {
-        return $this->deletePit($params);
-    }
-    /**
      * Returns the asyncSearch namespace
      */
     public function asyncSearch(): AsyncSearchNamespace
@@ -1706,6 +1691,13 @@ class Client
     public function knn(): KnnNamespace
     {
         return $this->knn;
+    }
+    /**
+     * Returns the ml namespace
+     */
+    public function ml(): MlNamespace
+    {
+        return $this->ml;
     }
     /**
      * Returns the monitoring namespace
@@ -1797,10 +1789,6 @@ class Client
     public function transforms(): TransformsNamespace
     {
         return $this->transforms;
-    }
-    public function ml(): MachineLearningNamespace
-    {
-        return $this->ml;
     }
 
     /**
--- src/OpenSearch/Namespaces/TasksNamespace.php
+++ src/OpenSearch/Namespaces/TasksNamespace.php
@@ -111,11 +111,4 @@ class TasksNamespace extends AbstractNamespace
 
         return $this->performRequest($endpoint);
     }
-    /**
-     * Proxy function to list() to prevent BC break since 7.4.0
-     */
-    public function tasksList(array $params = [])
-    {
-        return $this->list($params);
-    }
 }
